c17cf59a395f4fd643b734142a13ba956b2a96cf
tkerscher <88444139+tkerscher@users.noreply.github.com>
2023-09-02 04:53:48 +0200

    added test for philox rng

---
+++ tests/test_philox.py
@@ -0,0 +1,26 @@
+import numpy as np
+import hephaistos as hp
+
+def test_philox(shaderUtil):
+    # Config; Should match the one in the test shader
+    StreamSize = 10000
+    Streams = 4096
+    # allocate memory
+    buffer = hp.FloatBuffer(Streams * StreamSize)
+    tensor = hp.FloatTensor(Streams * StreamSize)
+
+    # create and run program
+    program = shaderUtil.createTestProgram("philox.test.glsl")
+    program.bindParams(outBuf=tensor)
+    (hp.beginSequence()
+        .And(program.dispatch(Streams // 32))
+        .And(hp.retrieveTensor(tensor, buffer))
+        .Submit())
+    
+    # checking randomness is a bit much for a unit test
+    # let's check for uniform instead
+    # NOTE: a simple linear increase would also pass this
+
+    hist = np.histogram(buffer.numpy(), bins=20)[0]
+    max_dev = np.abs((hist * 20.0 / len(buffer.numpy())) - 1.0).max()
+    assert max_dev < 0.005 #TODO: What is a reasonable value here?
