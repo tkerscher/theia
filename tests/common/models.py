d93e417cd9e28e023e6b0556f7b130863dc8a15b
tkerscher <88444139+tkerscher@users.noreply.github.com>
2024-05-04 02:13:04 +0200

    added polarization to water model in tests

--- tests/common/models.py
+++ tests/common/models.py
@@ -4,10 +4,14 @@ import theia.material
 class WaterModel(
     theia.material.WaterBaseModel,
     theia.material.HenyeyGreensteinPhaseFunction,
+    theia.material.KokhanovskyOceanWaterPhaseMatrix,
     theia.material.MediumModel,
 ):
     def __init__(self) -> None:
         theia.material.WaterBaseModel.__init__(self, 5.0, 1000.0, 35.0)
         theia.material.HenyeyGreensteinPhaseFunction.__init__(self, 0.6)
+        theia.material.KokhanovskyOceanWaterPhaseMatrix.__init__(
+            self, p90=0.66, theta0=0.25, alpha=4.0, xi=25.6  # voss measurement fit
+        )
 
     ModelName = "water"
