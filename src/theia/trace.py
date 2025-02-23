83c5ffda231ad0f5ed2c98efdabdb1b9ffff137c
tkerscher <88444139+tkerscher@users.noreply.github.com>
2025-02-23 19:18:16 +0100

    added missing entry in __all__ in trace.py

--- src/theia/trace.py
+++ src/theia/trace.py
@@ -29,6 +29,7 @@ __all__ = [
     "EventResultCode",
     "EventStatisticCallback",
     "SceneBackwardTracer",
+    "SceneBackwardTargetTracer",
     "SceneForwardTracer",
     "Tracer",
     "TraceEventCallback",
