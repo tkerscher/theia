679e9187b7e4237239ff2245e42c5714122b6ad5
tkerscher <88444139+tkerscher@users.noreply.github.com>
2026-02-01 20:35:06 +0100

    added missing sampler source code for light source targets
    unused in tests so not sure if working...

--- src/theia/shader/target/sample.glsl
+++ src/theia/shader/target/sample.glsl
@@ -1,6 +1,7 @@
-layout(local_size_x = 32) in;
+layout(local_size_x = 512) in;
 
 #include "target/common.glsl"
+#include "util/buffers.glsl"
 #include "util/sample.glsl"
 
 #include "rng.glsl"
@@ -54,32 +55,57 @@ struct Result {
     CompressedTargetSample hit;
     bool occluded;
 };
-writeonly buffer ResultBuffer{ Result r[]; };
 
-layout(scalar, push_constant) uniform Push {
+uniform SamplerParams {
+    uvec2 queueAdr;
+    uint queueSize;
+
     vec3 minObserver;
     vec3 maxObserver;
-};
+} samplerParams;
 
 void main() {
     uint i = gl_GlobalInvocationID.x;
-    if (i >= BATCH_SIZE) return;
+    if (i >= samplerParams.queueSize) return;
     uint dim = 0;
 
     //sample observer and direction
     vec3 observer = vec3(
-        mix(minObserver.x, maxObserver.x, random(i, dim)),
-        mix(minObserver.y, maxObserver.y, random(i, dim)),
-        mix(minObserver.z, maxObserver.z, random(i, dim))
+        mix(samplerParams.minObserver.x, samplerParams.maxObserver.x, random(i, dim)),
+        mix(samplerParams.minObserver.y, samplerParams.maxObserver.y, random(i, dim)),
+        mix(samplerParams.minObserver.z, samplerParams.maxObserver.z, random(i, dim))
     );
     vec3 direction = sampleUnitSphere(random2D(i, dim));
-
     //sample target
-    r[i] = Result(
-        observer,
-        direction,
-        checkSample(observer, sampleTarget(observer, i, dim)),
-        checkIntersect(observer, direction, intersectTarget(observer, direction)),
-        isOccludedByTarget(observer)
-    );
+    CompressedTargetSample sampl = checkSample(observer, sampleTarget(observer, i, dim));
+    CompressedTargetSample hit = checkIntersect(observer, direction, intersectTarget(observer, direction));
+
+    //save results
+    uint idx = gl_GlobalInvocationID.x;
+    uint queueSize = samplerParams.queueSize;
+    FloatBuffer floats = FloatBuffer(samplerParams.queueAdr);
+    UIntBuffer uints = UIntBuffer(samplerParams.queueAdr);
+    IntBuffer ints = IntBuffer(samplerParams.queueAdr);
+
+    #define _saveInt(v) ints.values[idx] = (v); idx += queueSize
+    #define _saveUInt(v) uints.values[idx] = (v); idx += queueSize
+    #define _saveFloat(v) floats.values[idx] = (v); idx += queueSize
+    #define _saveVec3(v) _saveFloat(v.x); _saveFloat(v.y); _saveFloat(v.z)
+
+    _saveVec3(observer);
+    _saveVec3(direction);
+
+    _saveVec3(sampl.position);
+    _saveVec3(sampl.normal);
+    _saveFloat(sampl.prob);
+    _saveUInt(uint(sampl.valid));
+    _saveInt(sampl.error);
+
+    _saveVec3(hit.position);
+    _saveVec3(hit.normal);
+    _saveFloat(hit.prob);
+    _saveUInt(uint(hit.valid));
+    _saveInt(hit.error);
+    
+    _saveUInt(uint(isOccludedByTarget(observer)));
 }
